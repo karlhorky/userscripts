b011dabdbdcd7a122d6240c88b5fb56730f0f5be
Karl Horky <karl.horky@gmail.com>
2024-07-19 12:19:41 +0200

    Fix more types

--- userscripts/autoclose-zoom-success-tabs.user.js
+++ userscripts/autoclose-zoom-success-tabs.user.js
@@ -8,16 +8,13 @@
 // @grant        window.close
 // ==/UserScript==
 
-// @ts-nocheck
-/* eslint-disable */
-
 setTimeout(() => {
   if (
     // https://us02web.zoom.us/j/6830992169#success
-    /^https:\/\/\S+\.zoom.us\/j\/.+#success$/.test(location.href) ||
+    /^https:\/\/\S+\.zoom.us\/j\/.+#success$/.test(window.location.href) ||
     // https://us02web.zoom.us/postattendee?id=8
     // https://us02web.zoom.us/postattendee
-    /^https:\/\/\S+\.zoom.us\/postattendee/.test(location.href)
+    /^https:\/\/\S+\.zoom.us\/postattendee/.test(window.location.href)
   ) {
     window.close();
   }
