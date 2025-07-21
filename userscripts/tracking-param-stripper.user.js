c82f7732e6fc16323dc8bd9e2281ceecf3a4015e
Karl Horky <karl.horky@gmail.com>
2025-07-21 19:45:40 +0200

    Add simple host matching, add rcm query parameter matching for LinkedIn

--- userscripts/tracking-param-stripper.user.js
+++ userscripts/tracking-param-stripper.user.js
@@ -2,7 +2,7 @@
 // @name         Tracking Param Stripper
 // @description  Drop the UTM params from a URL when the page loads.
 // @extra        Cuz you know they're all ugly n shit.
-// @version      1.4.0
+// @version      1.5.0
 // @author       Paul Irish, Karl Horky
 // @namespace    http://github.com/paulirish
 // @include      http*://*
@@ -11,23 +11,30 @@
 // From https://gist.github.com/paulirish/626834
 
 // Updates:
+// - 2025-07-21 Add matching for rcm (LinkedIn)
+// - 2025-07-21 Match hostnames
 // - 2025-07-20 Fix problems reported by ESLint and TS
-// - 2025-07-20 Added matching for mc_cid (Mailchimp)
-// - 2021-03-20 Added matching for vgo_ee (ActiveCampaign)
-// - 2020-02-07 Added matching for mkt_tok (Marketo)
-// - 2020-01-14 Added matching for ck_subscriber_id (ConvertKit)
+// - 2025-07-20 Add matching for mc_cid (Mailchimp)
+// - 2021-03-20 Add matching for vgo_ee (ActiveCampaign)
+// - 2020-02-07 Add matching for mkt_tok (Marketo)
+// - 2020-01-14 Add matching for ck_subscriber_id (ConvertKit)
 
-// You can install this user script by downloading this script,
-// going to about: extensions, turning on developer mode and dragging and dropping
-// this file onto the window. That'll install it.
-
-// lastly, if your site / marketing funnel uses these tracking tokens. you can clean up your users URLs
-// look at the comments below on correct installation to integrate with __gaq.push
-
-const trackingQueryParamPatterns =
-  'utm_|ck_subscriber_id|mc_cid|mkt_tok|vgo_ee';
+const trackingQueryParamPatterns = /** @type {[string, true | string[]][]} */ ([
+  ['ck_subscriber_id', true],
+  ['mc_cid', true],
+  ['mkt_tok', true],
+  ['rcm', ['www.linkedin.com']],
+  ['utm_', true],
+  ['vgo_ee', true],
+])
+  .filter(([, hosts]) => {
+    return hosts === true || hosts.includes(window.location.hostname);
+  })
+  .map(([pattern]) => pattern)
+  .join('|');
 
 if (
+  trackingQueryParamPatterns &&
   new RegExp(`(${trackingQueryParamPatterns})`).test(window.location.search)
 ) {
   // thx @cowboy for the revised hash param magic.
