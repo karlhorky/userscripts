d0388083d70b6f2a1abc807ed2cb3668df01513f
Karl Horky <karl.horky@gmail.com>
2025-09-11 14:03:26 +0200

    Switch back to button click to avoid GitHub 302 redirect with `.href =`

--- userscripts/disable-github-unread-notifications-view.user.js
+++ userscripts/disable-github-unread-notifications-view.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Disable GitHub Unread Notifications view
 // @description  Redirect away from Notifications "Unread" view
-// @version      1.1.2
+// @version      1.1.3
 // @author       Karl Horky
 // @namespace    https://www.karlhorky.com/
 // @match        https://github.com/notifications?query=*
@@ -9,5 +9,21 @@
 // ==/UserScript==
 
 const { searchParams } = new URL(document.location.href);
-searchParams.delete('query');
-document.location.href = `/notifications${searchParams.toString() ? `?${searchParams}` : ''}`;
+const queryParam = searchParams.get('query');
+
+if (queryParam === 'is:unread') {
+  const allButton = document.querySelector(
+    'ul[aria-label="Notification filters"].SegmentedControl button[name="query"][value=""]',
+  );
+  if (
+    allButton &&
+    allButton instanceof HTMLButtonElement &&
+    allButton.textContent.includes('All')
+  ) {
+    allButton.click();
+  }
+} else if (queryParam === '') {
+  // Disallow empty `query` param, eg. `?query=`
+  searchParams.delete('query');
+  document.location.href = `/notifications${searchParams.toString() ? `?${searchParams}` : ''}`;
+}
